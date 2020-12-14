0fda5643bed4a47ac75bfa59a69adf2623f53d41
Iana Chyrva <yanachirva@gmail.com>
2020-12-14 17:35:58 +0200

    Completed

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsOpenDoc = document.querySelectorAll('.show-modal');
+const btnClose = document.querySelector('.close-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenDoc.length; i++) {
+  btnsOpenDoc[i].addEventListener('click', openModal);
+}
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
